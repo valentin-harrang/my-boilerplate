eea8257706450d54c3dea6ff1684b35d3ff63ad1
Valentin Harrang <valhrg@gmail.com>
2024-12-18 22:54:39 +0100

    feat: spacing

--- src/components/form/password-forgotten.tsx
+++ src/components/form/password-forgotten.tsx
@@ -49,7 +49,7 @@ const PasswordForgottenForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 p-4">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
           name="email"
